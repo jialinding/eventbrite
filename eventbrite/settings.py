c9db213bd5b0a2138289c4354bf64ae4be12ca53
Jialin Ding <djialin09@gmail.com>
2015-03-23 22:29:10 -0700

    added database configuration

--- eventbrite/settings.py
+++ eventbrite/settings.py
@@ -91,7 +91,9 @@ TEMPLATE_DIRS = (
 """The following is copy/pasted from the Heroku Django guide"""
 # Parse database configuration from $DATABASE_URL
 import dj_database_url
-DATABASES['default'] =  dj_database_url.config()
+DATABASES = {}
+DATABASES['default'] =  dj_database_url.config(default='postgres://user:pass@host/db')
+DATABASES['default']['ENGINE'] = 'django.db.backends.postgresql_psycopg2'
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
