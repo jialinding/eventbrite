07b304e721974c9792baac7426440afd016e1b08
Jialin Ding <djialin09@gmail.com>
2015-03-24 00:51:15 -0700

    static files

--- codingchallenge/urls.py
+++ codingchallenge/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, url
+from django.conf import settings
 
 from codingchallenge import views
 
@@ -7,3 +8,8 @@ urlpatterns = patterns('',
     url(r'^choose/$', views.choose, name='choose'),
     url(r'^(?P<category_id_1>[0-9]+)/(?P<category_id_2>[0-9]+)/(?P<category_id_3>[0-9]+)/(?P<page>[0-9]+)/$', views.results, name='results'),
 )
+
+if not settings.DEBUG:
+    urlpatterns += patterns('',
+        (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
+    )
