a505126f739fb0b195168e6b2f1ba21d44e28435
Jialin Ding <djialin09@gmail.com>
2015-03-24 01:38:09 -0700

    debug to false

--- eventbrite/settings.py
+++ eventbrite/settings.py
@@ -22,7 +22,7 @@ SECRET_KEY = '=)01=wwv^p)9h362yh&x2=yvy140o_!=tkgx8(oc7fz)g$tq))'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-TEMPLATE_DEBUG = True
+TEMPLATE_DEBUG = False
 
 # Application definition
 
